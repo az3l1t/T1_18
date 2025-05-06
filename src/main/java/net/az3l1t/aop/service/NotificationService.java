ad7dbaff02c70099295a2978980ba57a6245e26f
az3l1t <eternity_cr9p@mail.ru>
2025-05-06 19:45:32 +0300

    v 1.0 -> fixed version

--- src/main/java/net/az3l1t/aop/service/NotificationService.java
+++ src/main/java/net/az3l1t/aop/service/NotificationService.java
@@ -1,16 +1,13 @@
 package net.az3l1t.aop.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import net.az3l1t.aop.config.notification.NotificationProperties;
-import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Log4j2
 public class NotificationService {
     private final JavaMailSender mailSender;
     private final NotificationProperties notificationProperties;
