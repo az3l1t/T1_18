d372327e9f5a6f7b18f5da386fc50e734b87540c
az3l1t <eternity_cr9p@mail.ru>
2025-05-06 15:00:21 +0300

    v 0.5 -> KafkaConfig -5 conf

--- src/main/java/net/az3l1t/aop/config/KafkaConfig.java
+++ src/main/java/net/az3l1t/aop/config/KafkaConfig.java
@@ -27,32 +27,22 @@ public class KafkaConfig {
     private String bootstrapServers;
     @Value("${kafka.consumer.group-id}")
     private String groupId;
-    @Value("${kafka.consumer.max-poll-records}")
-    private String maxPollRecords;
-    @Value("${kafka.consumer.max-partition-fetch-bytes}")
-    private String maxPartitionFetchBytes;
     @Value("${kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
     @Value("${kafka.consumer.enable-auto-commit}")
     private boolean enableAutoCommit;
-    @Value("${kafka.consumer.max-poll-interval-ms}")
-    private String maxPollIntervalMs;
-    @Value("${kafka.consumer.trusted-packages:*}")
-    private String trustedPackages;
     @Value("${kafka.producer.retries}")
-    private String retries;
+    private int retries;
     @Value("${kafka.listener.concurrency}")
-    private String concurrency;
+    private int concurrency;
     @Value("${kafka.listener.ack-mode}")
     private String ackMode;
     @Value("${kafka.listener.backoff-interval}")
     private long backoffInterval;
     @Value("${kafka.listener.backoff-max-attempts}")
     private long backoffMaxAttempts;
-    @Value("${kafka.consumer.fetch-min-bytes}")
-    private String fetchMinBytes;
-    @Value("${kafka.consumer.fetch-max-wait-ms}")
-    private String fetchMaxWaitMs;
+    @Value("${kafka.consumer.trusted-packages}")
+    private String trustedPackages;
 
     @Bean
     public ProducerFactory<String, KafkaUpdatingDto> producerFactory() {
@@ -60,7 +50,7 @@ public class KafkaConfig {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(ProducerConfig.RETRIES_CONFIG, Integer.parseInt(retries));
+        props.put(ProducerConfig.RETRIES_CONFIG, retries);
         return new DefaultKafkaProducerFactory<>(props);
     }
 
@@ -77,13 +67,8 @@ public class KafkaConfig {
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, Integer.parseInt(maxPollRecords));
-        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, Integer.parseInt(maxPartitionFetchBytes));
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit);
-        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, Integer.parseInt(maxPollIntervalMs));
         props.put(JsonDeserializer.TRUSTED_PACKAGES, trustedPackages);
-        props.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, Integer.parseInt(fetchMinBytes));
-        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, Integer.parseInt(fetchMaxWaitMs));
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
@@ -92,9 +77,9 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, KafkaUpdatingDto> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(concurrency);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.valueOf(ackMode));
         factory.setBatchListener(true);
-        factory.setConcurrency(Integer.parseInt(concurrency));
         return factory;
     }
 
