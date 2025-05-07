73688c477b72614a92e1e072cd61206fa70e020b
az3l1t <eternity_cr9p@mail.ru>
2025-05-07 07:58:40 +0300

    v 1.2

--- src/main/java/net/az3l1t/aop/config/notification/NotificationProperties.java
+++ src/main/java/net/az3l1t/aop/config/notification/NotificationProperties.java
@@ -1,11 +1,11 @@
 package net.az3l1t.aop.config.notification;
 
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Data
 @Component
+@Getter
 @ConfigurationProperties(prefix = "notification.email")
 public class NotificationProperties {
     private String sender;
