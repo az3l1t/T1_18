c2baf966adab6d0b3c953d16195eb8a62c2e221f
az3l1t <eternity_cr9p@mail.ru>
2025-05-12 14:47:03 +0300

    result after review -> ConfigurationPropertiesScan

--- src/main/java/net/az3l1t/aop/config/notification/AsyncNotificationProperties.java
+++ src/main/java/net/az3l1t/aop/config/notification/AsyncNotificationProperties.java
@@ -3,9 +3,7 @@ package net.az3l1t.aop.config.notification;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "async.notification")
